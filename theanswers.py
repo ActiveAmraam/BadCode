f9d0590094a083b3b2001043ce94ac9434efde9c
ActiveAmraam <silverdestruction666@gmail.com>
2024-08-12 17:40:31 +1000

    Add files via upload

---
+++ theanswers.py
@@ -0,0 +1,16 @@
+import sys
+import time
+print("Logging in...")
+time.sleep(2)
+print("Log in failed. \n Username: G.W.B")
+Password = str(input("Password: "))
+if Password == "Fallujah04":
+    print("Loading Enigma...")
+    time.sleep(5)
+    print("UKW C \n Rotor 1: I Position: 3C Ring: 24X \n Rotor 2: VIII Position: 18R Ring: 3C \n Rotor 3: VI Position: 12L Ring: 1A \n Plugboard: bq cr di ej kw mt os px uz gh \n Included foreign chars.")
+    time.sleep(20)
+    exit()
+else:
+    print("Password incorrect. You'll be kicked from the program.")
+    time.sleep(4)
+    exit()
